a28aff3e76f3792c1165350d2b5100f2a1e883d1
Quan Chao <clannad_2008@163.com>
2014-12-06 10:52:53 +0800

    fisrt commit
    Signed-off-by: Quan Chao <clannad_2008@163.com>

---
+++ arm/_build/radio_config.d
@@ -0,0 +1,14 @@
+.\_build\radio_config.o: ..\radio_config.c
+.\_build\radio_config.o: ..\radio_config.h
+.\_build\radio_config.o: ..\..\..\..\Include\nrf_delay.h
+.\_build\radio_config.o: ..\..\..\..\Include\nrf.h
+.\_build\radio_config.o: ..\..\..\..\Include\nrf51.h
+.\_build\radio_config.o: d:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cm0.h
+.\_build\radio_config.o: d:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\_build\radio_config.o: d:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmInstr.h
+.\_build\radio_config.o: d:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmFunc.h
+.\_build\radio_config.o: ..\..\..\..\Include\system_nrf51.h
+.\_build\radio_config.o: ..\..\..\..\Include\nrf51_bitfields.h
+.\_build\radio_config.o: d:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cm0.h
+.\_build\radio_config.o: ..\..\..\..\Include\nrf51_deprecated.h
+.\_build\radio_config.o: ..\..\..\..\Include\compiler_abstraction.h
